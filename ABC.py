368df53650a833bce63a88b9f6265e59b94dc8e8
Erum Nawaz <erum2406@gmail.com>
2020-09-20 22:00:44 +0530

    My First Python Program

---
+++ ABC.py
@@ -0,0 +1,25 @@
+n=int(input("Choose the type of Item: 1 for footwear / 2 for apperals" " "))
+print("You chosen :", n)
+if n == 1:    
+    f = float(input("Enter price of footwear per pair:"))
+    print("Price per pair of footwear is:", f)
+    if f<=500:
+        print("GST Rate is: 5%")
+        GSTAmount=(f*5/100)/100
+        NetPrice=f+GSTAmount
+    else:
+        print("GST Rate is: 18%")
+        GSTAmount=(f*18/100)/100
+        NetPrice=f+GSTAmount
+elif n==2:
+    a = float(input("Enter price of apperals per piece:"))
+    print("Price per piece of apperals is:", a)
+    if a<=1000:
+        print("GST Rate is: 5%")
+        GSTAmount=(a*5/100)/100
+        NetPrice=a+GSTAmount
+    else:
+        printf("GST Rate is: 12%")
+        GSTAmount=(a*12/100)/100
+        NetPrice=a+GSTAmount
+print("Total Selling Price is: ", NetPrice)
