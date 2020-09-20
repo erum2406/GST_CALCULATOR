9023a98676c64643cc0a130d8da6a3ec5da50726
Erum Nawaz <erum2406@gmail.com>
2020-09-20 22:10:07 +0530

    Updated per to /

--- ABC.py
+++ ABC.py
@@ -2,7 +2,7 @@ n=int(input("Choose the type of Item: 1 for footwear / 2 for apperals" " "))
 print("You chosen :", n)
 if n == 1:    
     f = float(input("Enter price of footwear per pair:"))
-    print("Price per pair of footwear is:", f)
+    print("Price /pair of footwear is:", f)
     if f<=500:
         print("GST Rate is: 5%")
         GSTAmount=(f*5/100)/100
@@ -13,7 +13,7 @@ if n == 1:
         NetPrice=f+GSTAmount
 elif n==2:
     a = float(input("Enter price of apperals per piece:"))
-    print("Price per piece of apperals is:", a)
+    print("Price /piece of apperals is:", a)
     if a<=1000:
         print("GST Rate is: 5%")
         GSTAmount=(a*5/100)/100
